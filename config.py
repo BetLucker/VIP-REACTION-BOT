360f1525270c9c005e9552fa959f3e9a3bb6f37b
BetLucker <fowziamujtaba@gmail.com>
2025-06-06 13:37:17 +0500

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
+BOT_TOKEN = getenv("BOT_TOKEN", "8083420393:AAEwlnMsPAsktUdHxB-BsC17LeFlISNQ5qM")
+MONGO_URL = getenv("MONGO_URL", " = "mongodb+srv://mikey:mikey@giyu.duescsi.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = int(getenv("OWNER_ID", "7775723899"))
 SUPPORT_GRP = "TG_FRIENDSS"
 UPDATE_CHNL = "VIP_CREATORS"
 OWNER_USERNAME = "THE_VIP_BOY"
